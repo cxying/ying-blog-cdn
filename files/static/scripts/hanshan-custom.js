7363a94f535bbb9f2d0c6014a138012c4e7934b9
Ying <46587259+luvying@users.noreply.github.com>
2020-09-16 22:23:31 +0800

    Update hanshan-custom.js

--- files/static/scripts/hanshan-custom.js
+++ files/static/scripts/hanshan-custom.js
@@ -40,7 +40,7 @@ function(_ref) {
 	timeout = window.requestAnimationFrame(function() {
 		var yValue = calcValue(y, window.innerHeight)+5;
 		var xValue = calcValue(x, window.innerWidth);
-		console.log(xValue, yValue);
+		//console.log(xValue, yValue);
 		background.style.backgroundPosition = xValue * .45 + "px " + (-yValue * .45 - 50) + "px";
 	})
 },false);
